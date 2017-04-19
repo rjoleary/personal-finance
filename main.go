aa82b560ef5bcd90a9b0b277ba1c5b89c172188e
Ryan O'Leary <ryan@ryanoleary.ca>
2017-04-19 16:43:05 -0400

    Added a few more commands

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -24,7 +25,7 @@ func main() {
 		log.Fatal("Usage: finance FILE...")
 	}
 
-	// Open database
+	// Open database.
 	db, err := sql.Open("sqlite3", "file:finance.db?_foreign_keys=1")
 	if err != nil {
 		log.Fatal(err)
@@ -34,79 +35,120 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if flag.Arg(0) == "list" {
-		rows, err := db.Query("SELECT acc_name, acc_currency FROM account")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-		for rows.Next() {
-			var name, currency string
-			err = rows.Scan(&name, &currency)
+	cmd, ok := map[string]func(){
+		"help": func() {
+			fmt.Println("list_accounts")
+			fmt.Println("list_trans ACCOUNT_NUMBER")
+			fmt.Println("create_account NAME")
+			fmt.Println("import FILE ACCOUNT_NUMBER")
+		},
+
+		"list_accounts": func() {
+			rows, err := db.Query("SELECT acc_id, acc_name, acc_currency FROM account")
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Println(name, currency)
-		}
-		err = rows.Err()
-		if err != nil {
-			log.Fatal(err)
-		}
-		return
-	}
-
-	if flag.Arg(0) == "account" {
-		_, err := db.Exec("INSERT INTO account(acc_name, acc_currency) VALUES (?, ?)", flag.Arg(1), flag.Arg(2))
-		if err != nil {
-			log.Fatal(err)
-		}
-		tx.Commit()
-		return
-	}
-
-	stmt, err := tx.Prepare(`
-INSERT INTO transact(trn_date, trn_amount, trn_description, acc_id) VALUES (?, ?, ?, 1)
-`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	for _, filename := range flag.Args() {
-		data, err := ioutil.ReadFile(filename)
-		if err != nil {
-			log.Printf("unable to read %q: %v", filename, err)
-			continue
-		}
-
-		// Identify the importer
-		var imp importer
-		for _, imp = range importers {
-			if imp.identify(data) {
-				break
+			defer rows.Close()
+			for rows.Next() {
+				var id int
+				var name, currency string
+				err = rows.Scan(&id, &name, &currency)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("%d %s (%s)\n", id, name, currency)
 			}
-		}
-		if imp == nil {
-			log.Printf("could not identify format of %q", filename)
-			continue
-		}
-
-		// Parse
-		trans, err := imp.parse(data)
-		if err != nil {
-			log.Printf("failed to parse %q as %q: %v", filename, imp.name(), err)
-			continue
-		}
-
-		// Insert to db
-		//j, _ := json.MarshalIndent(trans, "", "  ")
-		//log.Print(string(j))
-		for _, t := range trans {
-			if _, err = stmt.Exec(t.Date, t.Amount, t.Description); err != nil {
+			err = rows.Err()
+			if err != nil {
 				log.Fatal(err)
 			}
-		}
-		log.Printf("imported %q as %q", filename, imp.name())
+		},
+
+		"list_trans": func() {
+			rows, err := db.Query("SELECT trn_date, trn_amount, trn_description FROM transact NATURAL JOIN account WHERE acc_id == ?", flag.Args()[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer rows.Close()
+			for rows.Next() {
+				var date *time.Time
+				var amount *float64
+				var description *string
+				err = rows.Scan(&date, &amount, &description)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("(%v) ", date)
+				if amount != nil {
+					fmt.Printf("%.2f\t", *amount)
+				}
+				if description != nil {
+					fmt.Printf("%s", *description)
+				}
+				fmt.Println()
+			}
+			err = rows.Err()
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+
+		"create_account": func() {
+			_, err := db.Exec("INSERT INTO account(acc_name, acc_currency) VALUES (?, ?)", flag.Args()[1], flag.Args()[2])
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
+			return
+
+		},
+
+		"import": func() {
+			stmt, err := tx.Prepare(`
+		INSERT INTO transact(trn_date, trn_amount, trn_description, acc_id) VALUES (?, ?, ?, ?)
+		`)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+
+			filename := flag.Args()[1]
+			data, err := ioutil.ReadFile(filename)
+			if err != nil {
+				log.Fatalf("unable to read %q: %v", filename, err)
+			}
+
+			// Identify the importer
+			var imp importer
+			for _, imp = range importers {
+				if imp.identify(data) {
+					break
+				}
+			}
+			if imp == nil {
+				log.Fatalf("could not identify format of %q", filename)
+			}
+
+			// Parse
+			trans, err := imp.parse(data)
+			if err != nil {
+				log.Fatalf("failed to parse %q as %q: %v", filename, imp.name(), err)
+			}
+
+			// Insert to db
+			//j, _ := json.MarshalIndent(trans, "", "  ")
+			//log.Print(string(j))
+			for _, t := range trans {
+				if _, err = stmt.Exec(t.Date, t.Amount, t.Description, flag.Args()[2]); err != nil {
+					log.Fatal(err)
+				}
+			}
+			log.Printf("imported %q as %q", filename, imp.name())
+			tx.Commit()
+		},
+	}[flag.Arg(0)]
+	if !ok {
+		log.Fatal("command not found")
 	}
-	tx.Commit()
+	cmd()
 }
